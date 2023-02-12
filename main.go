59a801500f6963acc0e1f37df07670c864b66c10
ab <ab@my-mac-0.local>
2023-02-12 23:33:47 +0900

    implemented main function

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	fpath "example.com/me/filepath"
 )
+
+func eprintf(format string, a ...any) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
 
 func main() {
 	args := os.Args[1:]
 
 	for _, arg := range args {
 		tp := fpath.GetType(arg)
-		println(tp)
+		switch {
+		case tp.IsDir():
+			err := eraseDir(arg, os.Stderr)
+			if err != nil {
+				eprintf("%v\n", err)
+				return
+			}
+		case tp.IsExistingFile():
+			err := eraseFile(arg)
+			if err != nil {
+				eprintf("%v\n", err)
+				return
+			}
+		case tp.IsNotExisting():
+			eprintf("%v: not found\n", arg)
+			return
+		default:
+			panic("unsupported file type")
+		}
 	}
 }

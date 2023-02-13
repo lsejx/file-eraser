f63811b5981608dd6ab50c365b5d21d31556d8ff
ab <ab@my-mac-0.local>
2023-02-14 01:32:32 +0900

    implemented help-option and recursive-option

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	fpath "example.com/me/filepath"
 )
@@ -11,29 +12,54 @@ func eprintf(format string, a ...any) {
 	fmt.Fprintf(os.Stderr, format, a...)
 }
 
+var cmdName = filepath.Base(os.Args[0])
+
+var helpMes = fmt.Sprintf(`
+options:
+    %v help
+    -%c recursive
+    -%c interactive
+
+examples:
+    %v %v
+    %v file0 file1
+    %v -%c dir0 file0
+`, helpOpFull, recOp, intOp, cmdName, helpOpFull, cmdName, cmdName, recOp)
+
 func main() {
 	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Printf("%v: help\n", helpOpFull)
+		return
+	}
+	if args[0] == helpOpFull {
+		fmt.Print(helpMes)
+		return
+	}
 
+	op := newOption()
 	for _, arg := range args {
+		argIsOp := op.read(arg)
+		if argIsOp {
+			continue
+		}
 		tp := fpath.GetType(arg)
 		switch {
 		case tp.IsDir():
-			err := eraseDir(arg, os.Stderr)
-			if err != nil {
-				os.Exit(1)
+			if !op.recursive {
+				eprintf("%v: is a directory\n", arg)
+				continue
 			}
+			eraseDir(arg, os.Stderr)
 		case tp.IsExistingFile():
 			err := eraseFile(arg)
 			if err != nil {
 				eprintf("error: %v\n", err)
-				os.Exit(1)
 			}
 		case tp.IsNotExisting():
 			eprintf("%v: not found\n", arg)
-			os.Exit(1)
 		default:
-			eprintf("unsupported file type")
-			os.Exit(1)
+			eprintf("%v: unsupported file type\n", arg)
 		}
 	}
 }

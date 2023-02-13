3dea212f2e4ed24d47f0d1d5f7aa4878b1126e5a
ab <ab@my-mac-0.local>
2023-02-14 01:33:05 +0900

    prev commit forgot option.go

---
+++ option.go
@@ -0,0 +1,35 @@
+package main
+
+import "strings"
+
+const (
+	opPre      = '-'
+	helpOpFull = "-h"
+	recOp      = 'r'
+	intOp      = 'i'
+)
+
+type option struct {
+	recursive   bool
+	interactive bool
+}
+
+func newOption() option {
+	return option{false, false}
+}
+
+func (p *option) read(arg string) (changed bool) {
+	changed = false
+	if arg[0] != opPre {
+		return false
+	}
+	if strings.ContainsRune(arg, recOp) {
+		p.recursive = true
+		changed = true
+	}
+	if strings.ContainsRune(arg, intOp) {
+		p.interactive = true
+		changed = true
+	}
+	return changed
+}

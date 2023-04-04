63322c85975ec901db7f6030fc6b46b1b85d82fa
ab <ab@my-mac-0.local>
2023-04-04 16:02:11 +0900

    updated switch statement

--- main.go
+++ main.go
@@ -37,21 +37,19 @@ func main() {
 
 		tp := fpath.GetType(arg)
 		switch {
+		case tp.IsNotExisting():
+			eprintf("%v: not found\n", arg)
 		case tp.IsDir():
 			if !op.recursive {
 				eprintf("%v: is a directory\n", arg)
 				continue
 			}
 			eraseDir(arg, op.interactive, stdErr)
-		case tp.IsExisting():
+		default:
 			err := eraseFile(arg, op.interactive)
 			if err != nil {
 				eprintf("error: %v\n", err)
 			}
-		case tp.IsNotExisting():
-			eprintf("%v: not found\n", arg)
-		default:
-			eprintf("%v: unsupported file type\n", arg)
 		}
 	}
 }

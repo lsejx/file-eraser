9122e0fb73fd6813c18c4799934ab5a0c20f0fb9
ab <ab@my-mac-0.local>
2023-05-12 23:48:38 +0900

    update test

--- randomize_test.go
+++ randomize_test.go
@@ -1,19 +1,45 @@
 package main
 
-// func TestRandomize(t *testing.T) {
-// 	const length = 8
-// 	buf := bytes.NewBuffer(make([]byte, 0, length))
-// 	err := randomize(buf, length)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	t.Log(buf.Bytes())
-// }
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
 
-// func TestRamdomizeFile(t *testing.T) {
-// 	const path = "tmp.txt"
-// 	err := randomizeFile(path)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
+func TestRandomize(t *testing.T) {
+	tests := []struct {
+		id  string
+		buf []byte
+		l   int64
+	}{
+		{"nil", []byte{}, 0},
+		{"64", make([]byte, 0, 64), 64},
+		{"aiueo", []byte("aiueo")[:0], 5},
+	}
+	for _, tt := range tests {
+		r := bytes.NewBuffer(tt.buf)
+		err := randomize(r, tt.l)
+		if err != nil {
+			t.Fatalf("id:%v, err:%v", tt.id, err)
+		}
+		if int64(r.Len()) != tt.l {
+			t.Fatalf("id:%v, len:%v", tt.id, r.Len())
+		}
+		t.Logf("id:%v, buf:%v", tt.id, hex.EncodeToString(r.Bytes()))
+	}
+}
+
+func TestRandomizeFile(t *testing.T) {
+	tests := []struct {
+		path string
+		i    bool
+	}{
+		{"./test/randomize1.txt", false},
+	}
+	for _, tt := range tests {
+		err := randomizeFile(tt.path, tt.i)
+		if err != nil {
+			t.Fatalf("path:%v, err:%v", tt.path, err)
+		}
+	}
+}

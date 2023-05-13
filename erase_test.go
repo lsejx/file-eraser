3471944b65dd0448bdcd29c1c180c21c2a9e69ed
ab <ab@my-mac-0.local>
2023-05-13 23:33:05 +0900

    add test

--- erase_test.go
+++ erase_test.go
@@ -1,9 +1,55 @@
 package main
 
-// func TestEraseDir(t *testing.T) {
-// 	const path = "tmpdir"
-// 	err := eraseDir(path, os.Stderr)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
+import (
+	"testing"
+)
+
+func TestEraseFile(t *testing.T) {
+	tests := []struct {
+		p   string
+		o   option
+		err bool
+	}{
+		{"test/erase1.txt", option{false, false, false}, false},
+		{"test/erase2.txt", option{false, false, true}, false},
+		{"test/erase3.txt", option{false, false, false}, true},
+	}
+	for _, tt := range tests {
+		err := eraseFile(tt.p, tt.o)
+		if tt.err {
+			if err == nil {
+				t.Fatalf("path:%v, nilerr", tt.p)
+			}
+			t.Logf("path:%v, err:%v", tt.p, err)
+		} else {
+			if err != nil {
+				t.Fatalf("path:%v, err:%v", tt.p, err)
+			}
+		}
+	}
+}
+
+func TestEraseDir(t *testing.T) {
+	tests := []struct {
+		p string
+		o option
+		e bool
+	}{
+		{"test/eraseDir1", option{true, false, false}, false},
+		{"test/eraseDir2", option{true, false, true}, false},
+		{"test/eraseDir3", option{true, false, false}, true},
+	}
+	for _, tt := range tests {
+		err := eraseDir(tt.p, tt.o, stdErr)
+		if tt.e {
+			if err == nil {
+				t.Fatalf("path:%v, nilerr", tt.p)
+			}
+			t.Logf("path:%v, err:%v", tt.p, err)
+		} else {
+			if err != nil {
+				t.Fatalf("path:%v, err:%v", tt.p, err)
+			}
+		}
+	}
+}

11c60c6b3839fefd14dcb8796ca721897e9776eb
ab <ab@my-mac-0.local>
2023-05-14 00:31:01 +0900

    add test

---
+++ file_list_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestNewFileList(t *testing.T) {
+	fl := newFileList()
+	if fl.m == nil {
+		t.FailNow()
+	}
+	if len(fl.m) != 0 {
+		t.FailNow()
+	}
+}
+
+func TestIsNew(t *testing.T) {
+	fl := &fileList{m: make(map[string]byte)}
+	if !fl.isNew("test") {
+		t.Fatal(fl.m["test"])
+	}
+	if fl.isNew("test") {
+		t.FailNow()
+	}
+	if len(fl.m) != 1 {
+		t.Fatal(len(fl.m), fl.m)
+	}
+}
+
+func TestFileList(t *testing.T) {
+	fl := newFileList()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			if fl.isNew("test") {
+				t.Log("set", i)
+			} else {
+				t.Log(i)
+			}
+		}(i)
+	}
+	wg.Wait()
+}
